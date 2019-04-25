a40dad0457bbfb7188d67d82857bfbcfb2334491
zamarrowski <sergiozamarro@hotmail.com>
2019-04-25 22:27:33 +0200

    Added link to home in navbar

--- components/Appbar.js
+++ components/Appbar.js
@@ -2,12 +2,15 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import Link from 'next/link'
 
 export default props => 
   <AppBar position="static" color="primary">
     <Toolbar>
-      <Typography variant="h6" color="inherit">
-        Mergeamelo
-      </Typography>
+      <Link href="/">
+        <Typography variant="h6" color="inherit">
+          Mergeamelo
+        </Typography>
+      </Link>
     </Toolbar>
   </AppBar>
