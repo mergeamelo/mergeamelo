269cc4405014617b53c1f8b53cf36a6f79fd5054
zamarrowski <sergiozamarro@hotmail.com>
2019-04-29 22:46:56 +0200

    Mergeamelo roadmap link

--- components/Appbar.js
+++ components/Appbar.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Link from 'next/link'
+import IosMenu from 'react-ionicons/lib/IosMenu'
+import IconButton from '@material-ui/core/IconButton'
+import IosCompassOutline from 'react-ionicons/lib/IosCompassOutline'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
 
-export default props => 
-  <AppBar position="static" color="primary">
-    <Toolbar>
-      <Link href="/">
-        <Typography variant="h6" color="inherit">
-          Mergeamelo
+export default props => {
+  const [menuStatus, setMenuStatus] = useState(false)
+
+  return (
+    <>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <IconButton onClick={() => setMenuStatus(true)} style={{ marginLeft: '-20px' }} aria-label="Menu">
+            <IosMenu color="white" />
+          </IconButton>
+          <Link href="/">
+            <Typography variant="h6" color="inherit">
+              Mergeamelo
         </Typography>
-      </Link>
-    </Toolbar>
-  </AppBar>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={menuStatus} onClose={() => setMenuStatus(false)}>
+        <List>
+          <ListItem button>
+            <a href="https://github.com/mergeamelo/mergeamelo/projects" target="_blank">
+              <IosCompassOutline style={{ marginRight: '10px' }} /> Mergeamelo roadmap
+            </a>
+        </ListItem>
+        </List>
+      </Drawer>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,16 @@ import Appbar from '../components/Appbar'
 import PostsList from '../components/PostsList/PostsList'
 import Container from './../components/Container'
 import ContentContainer from '../components/ContentContainer'
-import MergeameloInfo from '../components/MergeameloInfo'
 
 import posts from './../components/PostsList/posts'
 
 export default props => {
+
   return (
     <>
       <Appbar/>
         <Container>
           <ContentContainer>
-            <MergeameloInfo />
             <PostsList posts={posts} />
           </ContentContainer>
         </Container>
