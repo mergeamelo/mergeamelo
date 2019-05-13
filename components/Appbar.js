2328834d76cfc5735680898ca7a7c6c69d8d397e
zamarrowski <sergiozamarro@hotmail.com>
2019-05-13 18:19:29 +0200

    Added new logo

--- components/Appbar.js
+++ components/Appbar.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 import IosMenu from 'react-ionicons/lib/IosMenu'
 import IconButton from '@material-ui/core/IconButton'
 import IosCompassOutline from 'react-ionicons/lib/IosCompassOutline'
-import Drawer from '@material-ui/core/Drawer'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 
@@ -15,27 +15,30 @@ export default props => {
 
   return (
     <>
-      <AppBar position="static" color="primary">
+      <AppBar position="static" style={{backgroundColor: 'black'}}>
         <Toolbar>
           <IconButton onClick={() => setMenuStatus(true)} style={{ marginLeft: '-20px' }} aria-label="Menu">
             <IosMenu color="white" />
           </IconButton>
           <Link href="/">
-            <Typography variant="h6" color="inherit">
-              Mergeamelo
-        </Typography>
+            <img style={{height: '68px'}} src="/static/images/logo.png"/>
           </Link>
         </Toolbar>
       </AppBar>
-      <Drawer open={menuStatus} onClose={() => setMenuStatus(false)}>
-        <List>
+      <SwipeableDrawer open={menuStatus} onClose={() => setMenuStatus(false)}>
+        <List style={{backgroundColor: 'black', height: '100%'}}>
           <ListItem button>
-            <a style={{textDecoration: 'none'}} href="https://github.com/mergeamelo/mergeamelo/projects" target="_blank">
-              <IosCompassOutline style={{ marginRight: '10px', verticalAlign: 'bottom' }} /> Mergeamelo roadmap
+            <a style={{textDecoration: 'none', color: 'white'}} href="https://github.com/mergeamelo/mergeamelo/projects" target="_blank">
+              <IosCompassOutline color="white" style={{ marginRight: '10px', verticalAlign: 'bottom' }} /> Mergeamelo roadmap
             </a>
-        </ListItem>
+          </ListItem>
+          <div style={{display: 'flex', justifyContent: 'center', height: '100%'}}>
+            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', marginBottom: '50px'}}>
+              <img src="/static/images/logo.png" />
+            </div>
+          </div>
         </List>
-      </Drawer>
+      </SwipeableDrawer>
     </>
   )
 }
